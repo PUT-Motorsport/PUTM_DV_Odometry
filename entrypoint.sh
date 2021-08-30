fc7ad03553ef07814657ddc12e17b6fc21cb8499
DawnKai <dawnwender@gmail.com>
2021-08-30 20:16:50 +0200

    create robot_localization node

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,15 +12,26 @@ cp -f /shared/CANsender.py ros2can/scripts/CANsender.py
 # Enable execution
 chmod +x ros2can/scripts/CANreceiver.py
 chmod +x ros2can/scripts/CANsender.py
+
 cd /program
+cp /shared/smb_navigation.bag /program/
+chmod +x smb_navigation.bag
+
+mv put_odometry src/put_odometry
+
+# Create launch files
+cd src/put_odometry
+mkdir launch
+cp /shared/config.launch launch/
+cp /shared/config.yaml launch/
+cd /program/
 
 # Build package
-mv put_odometry src/put_odometry
 catkin_make
 source devel/setup.bash
 
 
 # Run CAN
 roscore &
-rosrun ros2can CANreceiver.py &
-rosrun ros2can CANsender.py
+rosrun rviz rviz &
+roslaunch --wait put_odometry config.launch
